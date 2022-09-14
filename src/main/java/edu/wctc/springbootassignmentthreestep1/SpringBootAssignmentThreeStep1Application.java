d8a23f2e84116cd5f369803a427c7439708338db
Lynn Simonen <lynnsimonen@yahoo.com>
2022-09-13 21:22:02 -0500

    init commit

---
+++ src/main/java/edu/wctc/springbootassignmentthreestep1/SpringBootAssignmentThreeStep1Application.java
@@ -0,0 +1,13 @@
+package edu.wctc.springbootassignmentthreestep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAssignmentThreeStep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssignmentThreeStep1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/wctc/springbootassignmentthreestep1/SpringBootAssignmentThreeStep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wctc.springbootassignmentthreestep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssignmentThreeStep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
