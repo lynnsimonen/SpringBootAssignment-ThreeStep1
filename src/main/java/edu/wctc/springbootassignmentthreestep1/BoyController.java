77a9fa92d4c6d8d21040ca389b26c4c85c13c83f
Lynn Simonen <lynnsimonen@yahoo.com>
2022-09-14 17:19:48 -0500

    Add pages, links, content

--- src/main/java/edu/wctc/springbootassignmentthreestep1/BoyController.java
+++ src/main/java/edu/wctc/springbootassignmentthreestep1/BoyController.java
@@ -2,6 +2,7 @@ package edu.wctc.springbootassignmentthreestep1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoyController {
@@ -19,6 +20,11 @@ public class BoyController {
     @GetMapping("/step-three")
     public String getwyliePage() {
         return "steps/wylie";
+    }
+
+    @RequestMapping("family-cabin")
+    public String showCabin() {
+        return "redirect:https://www.campkeweenaw.com/index.html";
     }
 }
 
